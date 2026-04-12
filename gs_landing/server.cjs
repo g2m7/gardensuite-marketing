3b0c7fe03c844b0d1c58e1ea36464d33d32a99ab
zebcray98@gmail.com <63421403+g2m7@users.noreply.github.com>
2026-04-13 02:37:48 +0530

    Update server.cjs to port 3005

--- gs_landing/server.cjs
+++ gs_landing/server.cjs
@@ -1,6 +1,6 @@
 // Start script for PM2 to wrap the SvelteKit ESM build
-// Defaults to port 80 if not specified
-process.env.PORT = process.env.PORT || '80';
+// Defaults to port 3005 if not specified
+process.env.PORT = process.env.PORT || '3005';
 process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 
 import('./build/index.js').catch(err => {
